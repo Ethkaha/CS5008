a652205f3a787e28d8cf838d9810b811e134df38
Ethan Kahalany <m>
2026-01-18 18:41:26 -0500

    final update

--- HelloC/starterCode/FunctionPractice.c
+++ HelloC/starterCode/FunctionPractice.c
@@ -1,44 +1,48 @@
 /**
  * Starter Code Function Practice Code Along
- * 
+ *
  * This program demonstrates the use of functions in C.
- * 
+ *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h> 
+#include <stdint.h>
 
-int add(int a, int b) {
-return a + b;
+double add(double a, double b)
+{
+    return a + b;
 }
 
-int subtract(int a, int b) {
-return  a - b;
+double subtract(double a, double b)
+{
+    return a - b;
 }
 
-int multiply(int a, int b) {
-return a * b;
+double multiply(double a, double b)
+{
+    return a * b;
 }
 
-int divide(int a, int b) {
-	
-return a / b;
+double divide(double a, double b)
+{
+    return a / b;
 }
 
-int modulus(int a, int b) {
-return a % b;
+double modulus(int64_t a, int64_t b)
+{
+    return a % b;
 }
 
+int main()
+{
+    long a = 100;
+    short b = 6;
 
-int main() {
-	int a = 10;
-	int b = 5;
-	
-	printf("Additon: %d\n", add(a,b));
-	printf("Subtraction: %d\n", subtract(a,b));
-	printf("Multiply: %d\n", multiply(a,b));
-	printf("Divide: %d\n", divide(a,b));
+    printf("Addition: %.2f\n", add(a, b));
+    printf("Subtraction: %.2f\n", subtract(a, b));
+    printf("Multiplication: %.2f\n", multiply(a, b));
+    printf("Division: %.2f\n", divide(a, b));
+    printf("Modulus: %.0f\n", modulus(a, b));
     return 0;
 }
-
