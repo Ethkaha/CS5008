dd9430c7d3ac759146e259af927cdbdabd646de3
Ethan Kahalany <m>
2026-01-18 17:39:34 -0500

    function practice completed

--- HelloC/starterCode/FunctionPractice.c
+++ HelloC/starterCode/FunctionPractice.c
@@ -9,10 +9,36 @@
 #include <stdlib.h>
 #include <stdint.h> 
 
+int add(int a, int b) {
+return a + b;
+}
+
+int subtract(int a, int b) {
+return  a - b;
+}
+
+int multiply(int a, int b) {
+return a * b;
+}
+
+int divide(int a, int b) {
+	
+return a / b;
+}
+
+int modulus(int a, int b) {
+return a % b;
+}
+
 
 int main() {
-    
-
+	int a = 10;
+	int b = 5;
+	
+	printf("Additon: %d\n", add(a,b));
+	printf("Subtraction: %d\n", subtract(a,b));
+	printf("Multiply: %d\n", multiply(a,b));
+	printf("Divide: %d\n", divide(a,b));
     return 0;
 }
 
