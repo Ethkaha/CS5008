7dbe6f3d9062fae8b9fe25e8e5c9dd22a2663904
Ethan Kahalany <m>
2026-01-20 15:49:44 -0500

    condition practice walkthrough

--- HelloC/starterCode/ConditionPractice.c
+++ HelloC/starterCode/ConditionPractice.c
@@ -1,18 +1,79 @@
 /**
  * Starter Code for ConditionPractice Code Along
- * 
+ *
  * This program explores condition statements in C.
- * 
+ *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+void simpleConditionPractice(int lower, int upper)
+{
+    // if statement
+    if (lower < upper)
+    {
+        printf("Lower is less than upper.\n");
+    }
+    else if (lower > upper)
+    {
+        printf("Lower is greater than upper.\n");
+    }
+    else
+    {
+        printf("Lower is equal to upper.\n");
+    }
 
+    // one line if statement
+    if (lower < upper)
+        printf("Lower is less than upper (one line).\n");
+    else
+        printf("Lower is not less than upper\n");
+}
 
+void complexConditionPractice(int lower, int upper)
+{
+    // if with logical operatiors
+    if (lower < upper && lower > 0)
+    {
+        printf("Lower is less than upper and greater than zero\n");
+    }
+    else if (lower > upper || lower < 0)
+    {
+        printf("Lower is either greater than upper or less than zero\n");
+    }
+    else
+    {
+        printf("Lower is equal to upper and greater than or equal to zero\n");
+    }
 
+    // jist using int values
+    if (lower - upper)
+    {
+        printf("Lower is not equal to upper\n");
+    }
+    else
+    {
+        printf("Lower is equal to upper\n");
+    }
+}
 
-int main() {
+int main()
+{
+    int lower = -5;
+    int upper = 10;
 
+    printf("Simple Condition Practice with (%d, %d)\n", lower, upper);
+    simpleConditionPractice(lower, upper);
+    complexConditionPractice(lower, upper);
+
+    lower = 10;
+    upper = -10;
+
+    printf("Simple Condition Practice with (%d, %d)\n", lower, upper);
+    simpleConditionPractice(lower, upper);
+    complexConditionPractice(lower, upper);
+
+    printf("\n");
     return 0;
 }
